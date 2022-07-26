87f35c2c0ae2ffb99e705dd4c2adb54784fa0a98
Konstantin Honcharov <k.honcharov@gmail.com>
2022-07-27 01:38:55 +0300

    feaf: add tests for Board class

--- src/app/model/board.js
+++ src/app/model/board.js
@@ -2,42 +2,33 @@ class Board {
   constructor(width, height) {
     this.width = width
     this.height = height
-    this.board = this.create()
+    this.board = null
   }
 
-  create = () => {
-    const res = []
-    for (let i = 0; i <= this.width; i += 1) {
-      res.push([])
-      for (let j = 0; j <= this.height; j += 1) {
-        res[i].push(0)
+  create = (color = 0) => {
+    this.board = []
+    for (let i = 0; i < this.height; i += 1) {
+      this.board.push([])
+      for (let j = 0; j < this.width; j += 1) {
+        this.board[i].push(color)
       }
     }
-    return res
-  }
-
-  reset = () => {
-    this.board.forEach((row) => {
-      row.forEach((_, index) => {
-        row[index] = 0
-      })
-    })
   }
 
   merge = (shape) => {
-    const shiftX = shape.position.top
-    const shiftY = shape.position.left
-    for (let i = 0; i < shape.type.length; i += 1) {
-      for (let j = 0; j < shape.type[0].length; j += 1) {
+    const shiftX = shape.position.left
+    const shiftY = shape.position.top
+    for (let i = 0; i < shape.height; i += 1) {
+      for (let j = 0; j < shape.width; j += 1) {
         const color = shape.type[i][j]
         if (color) {
-          this.board[i + shiftX - 1][j + shiftY] = shape.type[i][j]
+          this.board[i + shiftY][j + shiftX] = color
         }
       }
     }
   }
 
-  findFullRows = () => {
+  _findFullRows = () => {
     const res = []
     this.board.forEach((row, index) => {
       const isFilled = row.every((item) => item > 0)
@@ -48,19 +39,22 @@ class Board {
     return res
   }
 
-  removeRow = (index) => {
-    const newArr = [...this.board.slice(0, index), ...this.board.slice(index + 1)]
-    newArr.unshift(new Array(this.board.width).fill(0))
-    return newArr
+  removeFullRows = () => {
+    const rowsToRemove = this._findFullRows()
+    rowsToRemove.forEach((index) => {
+      this.board = [...this.board.slice(0, index), ...this.board.slice(index + 1)]
+      this.board.unshift(new Array(this.width).fill(0))
+    })
+    return rowsToRemove
   }
 
   isOverflown = () => {
-    const OVERFLOW_ROW_INDEX = 3
+    const OVERFLOW_ROW_INDEX = 2
     return this.board[OVERFLOW_ROW_INDEX].some((item) => item > 0)
   }
 }
 
 export const BOARD_WIDTH = 17
-export const BOARD_HEIGHT = 29
+export const BOARD_HEIGHT = 17
 
 export const board = new Board(BOARD_WIDTH, BOARD_HEIGHT)
--- src/app/model/board.test.js
+++ src/app/model/board.test.js
@@ -2,12 +2,217 @@ import assert from 'assert/strict'
 
 import { board } from './board.js'
 
-/* Row removing */
+/* Create board */
 
-testBoard = [
+// 1
+board.width = 5
+board.height = 5
+board.create(8)
+assert.deepStrictEqual(board.board, [
+  [8, 8, 8, 8, 8],
+  [8, 8, 8, 8, 8],
+  [8, 8, 8, 8, 8],
+  [8, 8, 8, 8, 8],
+  [8, 8, 8, 8, 8],
+])
+
+// 2
+board.width = 1
+board.height = 1
+board.create(1)
+assert.deepStrictEqual(board.board, [[1]])
+
+// 3
+board.width = 4
+board.height = 2
+board.create(5)
+assert.deepStrictEqual(board.board, [
+  [5, 5, 5, 5],
+  [5, 5, 5, 5],
+])
+
+/* Shape merge */
+
+// 1
+let shape = {
+  width: 3,
+  height: 2,
+  type: [
+    [1, 1, 1],
+    [0, 1, 0],
+  ],
+  position: { top: 0, left: 0 },
+}
+
+board.width = 5
+board.height = 5
+board.create()
+board.merge(shape)
+
+assert.deepStrictEqual(board.board, [
+  [1, 1, 1, 0, 0],
+  [0, 1, 0, 0, 0],
+  [0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0],
+])
+
+// 2.1
+shape = {
+  width: 2,
+  height: 3,
+  type: [
+    [2, 2],
+    [0, 2],
+    [0, 2],
+  ],
+  position: { top: 1, left: 3 },
+}
+
+board.width = 5
+board.height = 4
+board.create()
+board.merge(shape)
+
+assert.deepStrictEqual(board.board, [
+  [0, 0, 0, 0, 0],
+  [0, 0, 0, 2, 2],
+  [0, 0, 0, 0, 2],
+  [0, 0, 0, 0, 2],
+])
+
+// 2.2
+shape = {
+  width: 2,
+  height: 2,
+  type: [
+    [3, 3],
+    [3, 3],
+  ],
+  position: { top: 2, left: 2 },
+}
+
+board.merge(shape)
+
+assert.deepStrictEqual(board.board, [
+  [0, 0, 0, 0, 0],
+  [0, 0, 0, 2, 2],
+  [0, 0, 3, 3, 2],
+  [0, 0, 3, 3, 2],
+])
+
+/* Find full row and remove */
+
+// 1
+board.width = 3
+board.height = 5
+board.board = [
   [3, 1, 5],
   [1, 4, 1],
-  [2, 2, 2],
+  [6, 2, 2],
+  [7, 2, 2],
+  [8, 2, 2],
 ]
+let rowsToRemove = board._findFullRows()
+assert.deepStrictEqual(rowsToRemove, [0, 1, 2, 3, 4])
 
-assert.deepEqual()
+let removedRows = board.removeFullRows()
+assert.deepStrictEqual(removedRows, rowsToRemove)
+assert.deepStrictEqual(board.board, [
+  [0, 0, 0],
+  [0, 0, 0],
+  [0, 0, 0],
+  [0, 0, 0],
+  [0, 0, 0],
+])
+
+// 2
+board.width = 3
+board.height = 5
+board.board = [
+  [3, 1, 5],
+  [0, 0, 1],
+  [6, 2, 2],
+  [7, 2, 2],
+  [0, 0, 2],
+]
+rowsToRemove = board._findFullRows()
+assert.deepStrictEqual(rowsToRemove, [0, 2, 3])
+
+removedRows = board.removeFullRows()
+assert.deepStrictEqual(removedRows, rowsToRemove)
+assert.deepStrictEqual(board.board, [
+  [0, 0, 0],
+  [0, 0, 0],
+  [0, 0, 0],
+  [0, 0, 1],
+  [0, 0, 2],
+])
+
+// 3
+board.width = 3
+board.height = 3
+board.board = [
+  [0, 0, 0],
+  [1, 4, 1],
+  [2, 2, 0],
+]
+rowsToRemove = board._findFullRows()
+assert.deepStrictEqual(rowsToRemove, [1])
+
+removedRows = board.removeFullRows()
+assert.deepStrictEqual(removedRows, rowsToRemove)
+assert.deepStrictEqual(board.board, [
+  [0, 0, 0],
+  [0, 0, 0],
+  [2, 2, 0],
+])
+
+// 4
+board.width = 3
+board.height = 3
+board.board = [
+  [0, 1, 0],
+  [0, 4, 1],
+  [2, 2, 0],
+]
+rowsToRemove = board._findFullRows()
+assert.deepStrictEqual(rowsToRemove, [])
+
+removedRows = board.removeFullRows()
+assert.deepStrictEqual(removedRows, rowsToRemove)
+assert.deepStrictEqual(board.board, [
+  [0, 1, 0],
+  [0, 4, 1],
+  [2, 2, 0],
+])
+
+/* Overflow check */
+
+// 1
+board.width = 3
+board.height = 5
+board.board = [
+  [0, 0, 0],
+  [0, 0, 0],
+  [0, 0, 0],
+  [0, 1, 0],
+  [1, 1, 1],
+]
+assert(board.isOverflown() === false)
+
+// 2
+board.width = 3
+board.height = 5
+board.board = [
+  [0, 0, 0],
+  [0, 0, 0],
+  [0, 0, 2],
+  [1, 0, 2],
+  [1, 2, 2],
+]
+assert(board.isOverflown() === true)
+
+/* Test result */
+
+console.log(`Board tests - \x1b[1;36mDONE\x1b[0m`)
